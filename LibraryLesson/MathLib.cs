ba6c939ff0b7d2cd5c3f848f0d832d639d10f8f1
Sarah George <sgeorge@tql.com>
2021-05-25 12:11:46 -0400

    Add project files.

---
+++ LibraryLesson/MathLib.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LibraryLesson
+{
+
+    public class MathLib
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Div(int a, int b)
+        {
+            return a / b;
+        }
+        public static int Modulo(int a, int b)
+        {
+            return Sub(a, Mul(Div(a, b), b));
+        }
+    }
+}
---
+++ TestLibrary/Program.cs
@@ -0,0 +1,18 @@
+﻿using LibraryLesson;
+using System;
+
+namespace TestLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var OnePlusTwo = MathLib.Add(1, 2);
+            var NbrMinNbr = MathLib.Sub(5, 3);
+            var NbrMulNbr = MathLib.Mul(2, 5);
+            var NbrDivNbr = MathLib.Div(6, 3);
+            var ans = MathLib.Modulo(5, 3);
+            
+        }
+    }
+}
